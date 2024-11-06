2cc7e06486b1cd7035a9593bda491425eb712857
ahmednour31234 <ahmednour312314@gmail.com>
2024-11-06 19:16:53 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [result, setresult] = useState("");
+
+const handleclick = (e) => {
+  e.preventDefault()
+    setresult(result.concat(e.target.name))
+}
+const clear = () => {
+  setresult("")
+}
+const backspace = () => {
+  setresult(result.slice(0,result.length-1))
+}
+const calculate = (e) => {
+  e.preventDefault()
+  try {
+    setresult(eval(result).toString())
+  } catch (error) {
+    setresult("Error")
+  }
+
+}
+  return (
+    
+    <div className="container">
+    
+      <form>
+        <input type="text" value={result} />
+        <div className="keypad">
+          <button className="highlight"  onClick={clear} id="clear">Clear</button>
+          <button className="highlight"  onClick={backspace} id="backspace">C</button>
+          <button className="highlight"  name="/" onClick={handleclick}>&divide;</button>
+          <button name="7" onClick={handleclick}>7</button>
+          <button name="8" onClick={handleclick}>8</button>
+          <button   name="9" onClick={handleclick}>9</button>
+          <button className="highlight" name="*" onClick={handleclick}>&times;</button>
+          <button name="4" onClick={handleclick}>4</button>
+          <button name="5" onClick={handleclick}>5</button>
+          <button name="6" onClick={handleclick}>6</button>
+          <button className="highlight"  name="-" onClick={handleclick}>&ndash;</button>
+          <button name="1" onClick={handleclick}>1</button>
+          <button name="2" onClick={handleclick}>2</button>
+          <button name="3" onClick={handleclick}>3</button>
+          <button className="highlight"  name="+" onClick={handleclick}>+</button>
+          <button name="0" onClick={handleclick}>0</button>
+          <button name="." onClick={handleclick}>.</button>
+          <button className="highlight" id="result"  onClick={calculate}>=</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
